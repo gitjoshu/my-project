c52d57274a03ffa2a91ca94de7f2e23d93286ee7
gitjoshu <joshua.munoz.barbero@gmail.com>
2022-05-17 18:19:49 +0200

    get .env database variables

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "@Quepasaepi9"),
+      database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+      user: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+      password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
