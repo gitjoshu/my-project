f89a7e519640590039c61bb3606127b02b1618fb
gitjoshu <joshua.munoz.barbero@gmail.com>
2022-05-24 17:41:19 +0200

    feat(docker)

--- frontend/apollo-client.js
+++ frontend/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4444/graphql",
+  uri: "http://localhost:4445/graphql",
   cache: new InMemoryCache(),
 });
 
